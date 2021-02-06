fefc851b1c1ce3c4e641bdcd2eb1eda63b9887eb
Adi <61146607+adi-kd0021@users.noreply.github.com>
2021-02-06 09:21:58 +0800

    Update Main.java
    Added comment for PR to nus-tic4002-AY2021S2/tp2

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -18,6 +18,10 @@ import javafx.application.Application;
  * By having a separate main class (Main) that doesn't extend Application
  * to be the entry point of the application, we avoid this issue.
  */
+
+/**
+  * NUS Expenses Tracker Main Launcher.
+  */
 public class Main {
     public static void main(String[] args) {
         Application.launch(MainApp.class, args);
