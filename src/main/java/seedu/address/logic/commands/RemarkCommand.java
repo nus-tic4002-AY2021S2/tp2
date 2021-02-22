8aea0774c014ce5709c4ce939a6a289936624906
ZhengShijie <ZhengShijie@users.noreply.github.com>
2021-02-22 20:18:12 +0800

    Update RemarkCommand.java

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,9 +1,10 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 /**
  * Changes the remark of an existing person in the address book.
