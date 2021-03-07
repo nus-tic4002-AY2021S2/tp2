6911707cc3125cf89bbe59572644455a1707f39b
Lin Binhui <>
2021-03-07 17:02:55 +0800

    Fixed send feature and implement advance send with personal message.

--- src/main/java/seedu/address/logic/commands/SendCommand.java
+++ src/main/java/seedu/address/logic/commands/SendCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.notification.SendEmail;
@@ -14,7 +17,7 @@ public class SendCommand extends Command {
             + "Parameters: Send INDEX e/[EMAIL]"
             + "example: send 1 e/hellokitty@hotmail.com";
 
-    public static final String MESSAGE_INVALID = "Inalid INPUT";
+    public static final String MESSAGE_INVALID = "Invalid range";
     public static final String MESSAGE_SUCCESS = "Email has been send";
 
     private String[] keywords;
@@ -28,22 +31,44 @@ public class SendCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         String email = "";
+        String message = "";
         int number = 0;
-        for (Object obj: keywords) {
+        for (Object obj : keywords) {
             if (obj.toString().contains("e/")) {
                 String[] data = obj.toString().split("e/", 2);
-                email = data[1].trim();
-            } else {
+                email = getEmail(data[1].trim());
+            } else if (obj instanceof Integer) {
                 number = Integer.parseInt(obj.toString().trim());
+
+            } else {
+                message = obj.toString().trim();
             }
         }
 
         if (number > model.getFilteredPersonList().size() || number <= 0) {
             return new CommandResult(String.format(MESSAGE_INVALID));
         } else {
-            new SendEmail(email, model.getFilteredPersonList().get(number - 1).toString());
+            if (email == "") {
+                email = getEmail(model.getFilteredPersonList().get(number - 1).toString());
+            }
+            if (message == "") {
+                message = model.getFilteredPersonList().get(number - 1).toString();
+            }
+            new SendEmail(email, message);
             return new CommandResult(
                     String.format(MESSAGE_SUCCESS));
         }
     }
+
+    private String getEmail(String email) throws CommandException {
+        String regex = ".*(\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b).*";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(email);
+        if (m.matches()) {
+            return m.group(1);
+        } else {
+            throw new CommandException("No valid email address found");
+        }
+    }
 }
+
