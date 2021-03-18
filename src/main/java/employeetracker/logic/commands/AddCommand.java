eb5fd0cd1bed6472f708950982c406616c9d28d1
e0260222 <e0260222@u.nus.edu>
2021-03-18 16:18:06 +0800

    Change sample command for Add command

--- src/main/java/employeetracker/logic/commands/AddCommand.java
+++ src/main/java/employeetracker/logic/commands/AddCommand.java
@@ -42,8 +42,8 @@ public class AddCommand extends Command {
             + PREFIX_DATE_OF_BIRTH + "1990-12-25 "
             + PREFIX_DATE_OF_JOINING + "2020-07-01 "
             + PREFIX_SALARY + "3000 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_TAG + "Founder "
+            + PREFIX_TAG + "l33tCoder";
 
     public static final String MESSAGE_SUCCESS = "New employee added: %1$s";
     public static final String MESSAGE_DUPLICATE_EMPLOYEE = "This employee already exists in Employee Tracker";
