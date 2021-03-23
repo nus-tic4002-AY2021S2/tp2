16c044217a3e382c9d3323c660960f25ed21b9b3
Yingtong <dgc5213@gmail.com>
2021-03-23 22:15:31 +0800

    Add assert

--- src/main/java/employeetracker/logic/parser/FindCommandParser.java
+++ src/main/java/employeetracker/logic/parser/FindCommandParser.java
@@ -41,6 +41,7 @@ public class FindCommandParser implements Parser<FindCommand> {
                         PREFIX_ROLE, PREFIX_ADDRESS, PREFIX_DATE_OF_BIRTH, PREFIX_PHONE);
 
         String trimmedArgs = args.trim();
+        assert trimmedArgs != null : "trimmedArgs is null";
 
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
