4387b9cf364b377930be8e56ac250c31be748f72
timmyly7 <e0261620@u.nus.edu>
2021-03-26 16:19:29 +0800

    Enhancement for Medical, Appointment Enhancement and UserGuide.md

--- src/main/java/seedu/address/logic/commands/AddMedicalCommand.java
+++ src/main/java/seedu/address/logic/commands/AddMedicalCommand.java
@@ -16,7 +16,13 @@ import seedu.address.model.person.medical.MedicalHistory;
 public class AddMedicalCommand extends Command {
     public static final String COMMAND_WORD = "addMed";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": add medical history to this person";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": To add medical history to patient, please enter"
+            + " the id of the person you want to add \nmedical history, and follow by '\\d +  history description'"
+            + " For example : \n"
+            + "usage 'addMed 1 \\d Two month of Panandol'";
+
+    public static final String SUCCESS_MESSAGE_USAGE = COMMAND_WORD + ": Medical history has been successfully added"
+            + " to this patient.";
 
     private final Index targetIndex;
 
@@ -47,15 +53,18 @@ public class AddMedicalCommand extends Command {
         Person personToAddMed = lastShownList.get(targetIndex.getZeroBased());
 
         Person editedPerson = personToAddMed;
-        Integer maxIndex = editedPerson.getMedicalHistories().stream()
-                .max(Comparator.comparing(v -> v.getIndex())).get().getIndex();
+        Integer maxIndex = 0;
+        if (editedPerson.getMedicalHistories().size() != 0) {
+            maxIndex = editedPerson.getMedicalHistories().stream()
+                    .max(Comparator.comparing(v -> v.getIndex())).get().getIndex();
+        }
 
         editedPerson.getMedicalHistories().add(new MedicalHistory(description, ++maxIndex));
         model.setPerson(personToAddMed, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
 
-        return new CommandResult(String.format(MESSAGE_USAGE, personToAddMed),
-                MESSAGE_USAGE);
+        return new CommandResult(String.format(SUCCESS_MESSAGE_USAGE, personToAddMed),
+                SUCCESS_MESSAGE_USAGE);
     }
 
 }
