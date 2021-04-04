81d2b46f177cf440c9fed682c0e5d576c89823da
Tay Jun Wen <e0261613@u.nus.edu.sg>
2021-04-04 13:46:38 +0800

    fixed 2 bug in create group

--- src/main/java/seedu/address/logic/parser/CreateGroupParser.java
+++ src/main/java/seedu/address/logic/parser/CreateGroupParser.java
@@ -1,8 +1,16 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddGroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.group.Group;
@@ -20,6 +28,11 @@ public class CreateGroupParser {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_GROUP);
 
+        if (!arePrefixesPresent(argMultimap, PREFIX_GROUP)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddGroupCommand.MESSAGE_USAGE));
+        }
+
         Group group = null;
         if (argMultimap.getValue(PREFIX_GROUP).isPresent()) {
             group = ParserUtil.parseGroup(argMultimap.getValue(PREFIX_GROUP).get());
@@ -27,5 +40,9 @@ public class CreateGroupParser {
 
         return new AddGroupCommand(group);
 
+    }
+
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
